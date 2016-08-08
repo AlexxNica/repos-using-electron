282c4cd96e12ad160838b40df357336c6a951b79
Zeke Sikelianos <zeke@sikelianos.com>
2016-08-07 21:57:11 -0700

    file repo files with package.json data from github

--- fill.js
+++ fill.js
@@ -1,26 +1,32 @@
 require('dotenv').load()
 
 const repos = require('object-values')(require('require-dir')('./repos'))
-
-console.log(repos)
+const fs = require('fs')
 const path = require('path')
 const RateLimiter = require('limiter').RateLimiter
 const limiter = new RateLimiter(1, 1000)
 const getPackageJSON = require('get-repo-package-json')
+const exists = require('path-exists').sync
+
+console.log(`Filling ${repos.length} repos with metadata`)
 
 repos.forEach(repo => {
-  // For now, skip repos that already have some metadata
-  if (repo.name) return
 
-  let filename = path.join(__dirname, 'repos', `${repo._librariesioMetadata.full_name.replace('/', '___')}.json`)
+  let full_name = repo._librariesioMetadata.full_name
+  let filename = path.join(__dirname, 'repos', `${full_name.replace('/', '___')}.json`)
 
-  // console.log(repo.full_name)
+  // skip repos that already have package.json metadata
+  if (repo.name) {
+    return console.log(`${full_name} (skip)`)
+  } else {
+    console.log(full_name)
+  }
+
   limiter.removeTokens(1, function () {
-    getPackageJSON(repo.full_name, function (err, pkg) {
+    getPackageJSON(full_name, function (err, pkg) {
       if (err) throw err
-      pkg._librariesioMetadata = repo
-      let filename = path.join(__dirname, 'repos', `${repo.full_name.replace('/', '___')}.json`)
-      console.log('  ' + filename)
-      fs.writeFileSync(filename, JSON.stringify(pkg, null, 2))
+      let extant = exists(filename) ? require(filename) : {}
+      fs.writeFileSync(filename, JSON.stringify(Object.assign({}, extant, pkg), null, 2))
     })
   })
+})
