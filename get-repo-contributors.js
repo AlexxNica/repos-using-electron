1c057503d33a5aa7d1879369102a68af54f800b5
Zeke Sikelianos <zeke@sikelianos.com>
2016-09-06 22:34:00 -0700

    get repo contributors

---
+++ get-repo-contributors.js
@@ -0,0 +1,35 @@
+require('dotenv').load()
+
+const repos = require('require-dir')('./repos')
+const fs = require('fs')
+const path = require('path')
+const URL = require('url')
+const RateLimiter = require('limiter').RateLimiter
+const limiter = new RateLimiter(1, 1000)
+var Octokat = require('octokat')
+var octo = new Octokat({token: process.env.GITHUB_ACCESS_TOKEN})
+
+console.log(`Filling ${Object.keys(repos).length} repos with contributor data`)
+
+Object.keys(repos).forEach(basename => {
+  let [owner, name] = basename.split('___')
+  let filename = path.join(__dirname, 'repos', `${basename}.json`)
+  let repo = require(filename)
+
+  // bail if metadata is already present
+  if (repo.status === 404 || repo.contributors) return
+
+  limiter.removeTokens(1, function () {
+    octo.repos(owner, name).contributors.fetch({per_page: 100, limit: 100})
+      .then(users => {
+        repo.contributors = users.map(user => user.login)
+        console.log(repo.contributors)
+        saveFile(repo, filename)
+      })
+  })
+})
+
+function saveFile (repo, filename) {
+  console.log(`${path.basename(filename)} (${repo.status})`)
+  fs.writeFileSync(filename, JSON.stringify(repo, null, 2))
+}
