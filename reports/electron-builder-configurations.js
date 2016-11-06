0e3d415b3d1c2d95569a0aff2ad48053787ff603
Zeke Sikelianos <zeke@sikelianos.com>
2016-11-05 21:22:45 -0700

    display scripts stanzas too

--- reports/electron-builder-configurations.js
+++ reports/electron-builder-configurations.js
@@ -1,9 +1,19 @@
 const repos = require('..').filter(repo => !repo.fork)
 const dedent = require('dedent')
 
+console.log(dedent`
+  # \`electron-builder\` configurations
+
+  A list of repositories that depend on \`electron-rebuild\`, sorted by
+  repository fork count. Each entry displays the value of the \`build\` and
+  \`scripts\` stanzas from the repo's \`package.json\`.
+`)
+
+
 repos
   .filter(repo => repo.pkg.somehowDependsOn('electron-builder'))
   .filter(repo => repo.pkg.build)
+  .filter(repo => repo.pkg.scripts)
   .forEach(repo => {
     console.log(dedent`
 ### [${repo.fullName}](https://github.com/${repo.fullName}/tree/master/package.json)
@@ -13,5 +23,11 @@ repos
 \`\`\`json
   ${JSON.stringify(repo.pkg.build, null, 2)}
 \`\`\`
+
+\`\`\`json
+  ${JSON.stringify(repo.pkg.scripts, null, 2)}
+\`\`\`
+
+
 \n\n\n`)
   })
